b0225fc3b095c70aad624850adb4da084f94a0bc
이서준 <leeseojun713@gmail.com>
2022-04-25 17:01:14 +0900

    third commit
    HTML태그 제거 절차 간소화 및 오류 수정

--- crawling.py
+++ crawling.py
@@ -11,25 +11,10 @@ if response.status_code == 200:
     html = response.text
     soup = BeautifulSoup(html, 'html.parser')
     content = soup.select_one('#container > div.main_content > div.meal_menu > ul > li')
-    content = str(content)
-    content = content.split('<br>')
+    lunch_menu = content.find_all(text=True) # 텍스트만 찾아서 추출 후 리스트 형태로 저장
 
-    # 불필요한 문자열(HTML TAG) 제거
-    for i in content :
-        if '<br/>' in i :
-            i = i.split("<br/>")
-            for j in i :
-                if '</br>' in j :
-                    print(j.strip("</br></br></li>"))
-                elif '</li>' in j :
-                    print(j.strip("</li>"))
-                else :
-                    print(j)
-        if '</li>' in i :
-            print(i.strip("</li>"))
-        elif '<li>' in i :
-            print(i[4:])
-        
+    for i in lunch_menu :
+        print(i)
 
 # 크롤러가 웹 사이트에 접근할 수 없다면 status code 출력
 else : 
